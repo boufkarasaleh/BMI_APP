4e5436ea9195c5089d6fff345558099652cfe0f1
Saleh <boufkarasaleh@gmail.com>
2021-08-09 15:13:40 +0100

    adding the app code

--- BMI.py
+++ BMI.py
@@ -1,2 +1,55 @@
-# Welcome to the BIM APP
+# Welcome to the BIM calculator APP
+# The app will ask you about your height and weight
+height = float(input("what is your height in metters: "))
+weight = float(input("what is your weight in kg: "))
+
+# The calculation is : BMI = weight / height ** 2
+BMI_result = round(weight / height ** 2, 2)
+
+# The app conditions
+if BMI_result < 18.5: # Under 18.5 they are underweight
+    print(f"your BMI is {BMI_result}, you are underweight")
+elif BMI_result >= 18.5: # Over 18.5 but below 25 they have a normal weight
+    print(f"your BMI is {BMI_result}, you have a normal weight")
+elif BMI_result >= 25: # Over 25 but below 30 they are overweight
+    print(f"your BMI is {BMI_result}, you are overweight")
+elif BMI_result >= 30: # Over 30 but below 35 they are obese
+    print(f"your BMI is {BMI_result}, you are obese")
+elif BMI_result >= 35: # Above 35 they are clinically obese
+    print(f"your BMI is {BMI_result}, you are clinically obese")    
+else:
+    print("please enter the value of your height and weight")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
